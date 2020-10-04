8fb92694a08e4ce206271bc670748c95f4a70728
lingchen336 <473531303@qq.com>
2020-10-04 11:15:24 +0800

    在范围0～n-1内的n个数字中有且只有一个数字不在该数组中，请找出这个数字

---
+++ src/com/test/algorithm/jianzhi/O_53_II.java
@@ -0,0 +1,27 @@
+package com.test.algorithm.jianzhi;
+
+/*
+一个长度为n-1的递增排序数组中的所有数字都是唯一的，并且每个数字都在范围0～n-1之内。
+在范围0～n-1内的n个数字中有且只有一个数字不在该数组中，请找出这个数字。
+
+ */
+
+class O_53_II {
+    public static int missingNumber(int[] nums) {
+        int l=0,r=nums.length-1;
+        while(l<=r){
+            int mid=(l+r)/2;
+            if(nums[mid]==mid){
+                l=mid+1;
+            }else{
+                r=mid-1;
+            }
+        }
+        return l;
+    }
+
+    public static void main(String[] args) {
+        int a[]={0};
+        System.out.print(missingNumber(a));
+    }
+}
