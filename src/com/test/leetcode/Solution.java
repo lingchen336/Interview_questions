f3cb82821b0a9b5d12395662deeff93680a63014
lingchen336 <473531303@qq.com>
2020-10-06 17:47:49 +0800

    算法练习

--- src/com/test/leetcode/Solution.java
+++ src/com/test/leetcode/Solution.java
@@ -1,23 +1,23 @@
 package com.test.leetcode;
+/*
+给定一个数组 A[0,1,…,n-1]，请构建一个数组 B[0,1,…,n-1]，其中 B 中的元素 B[i]=A[0]×A[1]×…×A[i-1]×A[i+1]×…×A[n-1]。
+不能使用除法。
 
-
-import java.util.*;
+ */
 
 class Solution {
-    public int singleNumber(int[] nums) {
-        if(nums.length==0)return -1;
-        int bitSum[]=new int[32];
-        int res=0;
-        for(int num : nums){
-            int bitMask=1;
-            for (int i = 31; i >=0 ; i--) {
-                if((num&bitMask)!=0)bitSum[i]++;
-                bitMask=bitMask<<1;
-            }
+    public int[] constructArr(int[] a) {
+        if(a==null||a.length==0)return new int[0];
+        int[] res = new int[a.length];
+        int left = 1;
+        for (int i = 0; i < res.length; i++) {
+            res[i] =left;
+            left *= a[i];
         }
-        for (int i = 0; i < 32; i++) {
-            res=res<<1;
-            res+= bitSum[i]%3;
+        int right = 1;
+        for (int i = res.length - 1; i >= 0; i--) {
+            res[i] *= right;
+            right *= a[i];
         }
         return res;
     }
--- src/com/test/temp/Test.java
+++ src/com/test/temp/Test.java
@@ -1,45 +1,49 @@
 package com.test.temp;
 
 /*
-quickSort 快速排序。。。
+
  */
-
-import java.sql.SQLOutput;
-
-public class Test{
-    public static void main(String[] args) {
-        int[] arr = { 49, 38, 65, 97, 23, 22, 76, 1, 5, 8, 2, 0, -1, 22 };
-        quickSort(arr,0,arr.length-1);
-        System.out.println("排序后：");
-        for (int i:arr
-             ) {
-            System.out.println(i);
-        }
+class Solution {
+    private int index = 0;//全局索引
+    private boolean scanUnsignedInteger(String str) {
+        //是否包含无符号数
+        int before = index;
+        while(str.charAt(index) >= '0' && str.charAt(index) <= '9')
+            index++;
+        return index > before;
     }
-
-    private static void quickSort(int []arr,int low,int high){
-        if(low<high){
-            int index=getIndex(arr,low,high);
-
-            quickSort(arr,low,index-1);
-            quickSort(arr,index+1,high);
-        }
+    private boolean scanInteger(String str) {
+        //是否包含有符号数
+        if(str.charAt(index) == '+' || str.charAt(index) == '-')
+            index++;
+        return scanUnsignedInteger(str);
     }
-
-    private static int getIndex(int []arr,int low,int high){
-        int tmp=arr[low];
-        while(low<high){
-            while(low<high&&arr[high]>=tmp){
-                high--;
-            }
-            arr[low]=arr[high];
-            while(low<high&&arr[low]<=tmp){
-                low++;
-            }
-            arr[high]=arr[low];
+    public boolean isNumber(String s) {
+        //空字符串
+        if(s == null || s.length() == 0)
+            return false;
+        //添加结束标志
+        s = s + '|';
+        //跳过首部的空格
+        while(s.charAt(index) == ' ')
+            index++;
+        boolean numeric = scanInteger(s); //是否包含整数部分
+        if(s.charAt(index) == '.') {
+            index++;
+            //有小数点，处理小数部分
+            //小数点两边只要有一边有数字就可以，所以用||，
+            //注意scanUnsignedInteger要在前面，否则不会进
+            numeric = scanUnsignedInteger(s) || numeric;
         }
-
-        arr[low]=tmp;
-        return low;
+        if((s.charAt(index) == 'E' || s.charAt(index) == 'e')) {
+            index++;
+            //指数部分
+            //e或E的两边都要有数字，所以用&&
+            numeric = numeric && scanInteger(s);
+        }
+        //跳过尾部空格
+        while(s.charAt(index) == ' ')
+            index++;
+        return numeric && s.charAt(index) == '|' ;
     }
 }
