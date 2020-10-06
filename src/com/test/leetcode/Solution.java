97ff69ce3652be206a138b3b7ed29372418ac064
lingchen336 <473531303@qq.com>
2020-10-06 20:53:00 +0800

    n个骰子的点数

--- src/com/test/leetcode/Solution.java
+++ src/com/test/leetcode/Solution.java
@@ -1,54 +1,24 @@
 package com.test.leetcode;
 /*
-定义栈的数据结构，请在该类型中实现一个能够得到栈的最小元素的 min 函数在该栈中，调用 min、push 及 pop 的时间复杂度都是 O(1)。
+把n个骰子扔在地上，所有骰子朝上一面的点数之和为s。输入n，打印出s的所有可能的值出现的概率。
+你需要用一个浮点数数组返回答案，其中第 i 个元素代表这 n 个骰子所能掷出的点数集合中第 i 小的那个的概率。
 
-示例:
-
-MinStack minStack = new MinStack();
-minStack.push(-2);
-minStack.push(0);
-minStack.push(-3);
-minStack.min();   --> 返回 -3.
-minStack.pop();
-minStack.top();      --> 返回 0.
-minStack.min();   --> 返回 -2.
+例如:
+输入: 1
+输出: [0.16667,0.16667,0.16667,0.16667,0.16667,0.16667]
  */
-
-class MinStack {
-
-    /** initialize your data structure here. */
-    private ListNode head;
-    public MinStack() {
-        head=null;
-    }
-
-    public void push(int x) {
-        if(head==null) head=new ListNode(x,x,null);
-        else {
-            head=new ListNode(x,Math.min(x,head.min),head);
+class Solution {
+    public double[] twoSum(int n) {
+        double pre[]={1/6d,1/6d,1/6d,1/6d,1/6d,1/6d};
+        for (int i = 2; i <= n; i++) {
+            double temp[]=new double[5*i+1];
+            for (int j = 0; j < pre.length; j++) {
+                for (int k = 0; k < 6; k++) {
+                    temp[j+k]+=pre[j]/6;
+                }
+            }
+            pre=temp;
         }
-    }
-
-    public void pop() {
-        head=head.next;
-    }
-
-    public int top() {
-        return head.val;
-    }
-
-    public int min() {
-        return head.min;
-    }
-
-    class ListNode{
-        int val;
-        int min;
-        ListNode next;
-        public ListNode(int val,int min,ListNode next){
-            this.val=val;
-            this.min=min;
-            this.next=next;
-        }
+        return pre;
     }
 }
