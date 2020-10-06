631992441cfa0e03b3fe80c0a72b953c3a7981a4
lingchen336 <473531303@qq.com>
2020-10-06 23:20:12 +0800

    数组中只出现一次的数字

--- src/com/test/algorithm/jianzhi/O_56.java
+++ src/com/test/algorithm/jianzhi/O_56.java
@@ -1,6 +1,10 @@
 package com.test.algorithm.jianzhi;
+/*
+数组中只出现一次的数字
+一个整型数组 nums 里除两个数字之外，其他数字都出现了两次。请写程序找出这两个只出现一次的数字。要求时间复杂度是O(n)，空间复杂度是O(1)。
+ */
 
-public class o_56 {
+public class O_56 {
     public int[] singleNumbers(int[] nums) {
         int sum=0;
         int []res=new int[2];
