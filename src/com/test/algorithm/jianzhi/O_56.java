228c442fb4d02806f94f48a6f1abff55f0391368
lingchen336 <473531303@qq.com>
2020-10-06 23:56:13 +0800

    数组中只出现一次的数字

--- src/com/test/algorithm/jianzhi/O_56.java
+++ src/com/test/algorithm/jianzhi/O_56.java
@@ -11,8 +11,8 @@ public class O_56 {
         for (int i = 0; i < nums.length; i++) {
             sum^= nums[i];
         }
-        int lowbit=sum&(-sum);
-        for( int num:nums ){
+        int lowbit=sum&(-sum); //由于两个数异或的结果就是两个数数位不同结果的直观表现，所以我们可以通过异或后的结果去找 mask！
+        for( int num:nums ){                    //我们可以把数组分为两组进行异或，那么就可以知道是哪两个数字不同了。 两个不同的数字分别在两组中。
             if((num&lowbit)==0) res[0]^=num;
             else res[1]^=num;
         }
