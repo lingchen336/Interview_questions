859c97767f07f153a093324cb35c1a44343b34d8
lingchen336 <473531303@qq.com>
2020-10-07 16:19:57 +0800

    对称的二叉树

--- src/com/test/algorithm/BiTree/O_28.java
+++ src/com/test/algorithm/BiTree/O_28.java
@@ -1,6 +1,11 @@
 package com.test.algorithm.BiTree;
 /*
 请实现一个函数，用来判断一棵二叉树是不是对称的。如果一棵二叉树和它的镜像一样，那么它是对称的。
+题解：
+对称二叉树定义： 对于树中 任意两个对称节点 L 和 R，一定有：
+    L.val = R.valL.val=R.val ：即此两对称节点值相等。
+    L.left.val = R.right.valL.left.val=R.right.val ：即 L 的 左子节点 和 R 的 右子节点 对称；
+    L.right.val = R.left.valL.right.val=R.left.val ：即 L 的 右子节点 和 R 的 左子节点 对称。
 
  */
 
