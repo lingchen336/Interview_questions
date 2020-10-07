1fc3793fbe42b8f49f681fda1b04e2b33b1848e2
lingchen336 <473531303@qq.com>
2020-10-07 15:49:15 +0800

    树的深度

---
+++ src/com/test/algorithm/BiTree/O_55_II.java
@@ -0,0 +1,23 @@
+package com.test.algorithm.BiTree;
+
+/*
+
+ */
+class O_55_II {
+     public class TreeNode {
+         int val;
+         TreeNode left;
+         TreeNode right;
+
+         TreeNode(int x) {
+             val = x;
+         }
+     }
+
+    public int maxDepth(TreeNode root) {
+         if(root==null)return 0;
+         int left=maxDepth(root.left);
+         int right=maxDepth(root.right);
+         return Math.max(left, right)+1;
+    }
+}
