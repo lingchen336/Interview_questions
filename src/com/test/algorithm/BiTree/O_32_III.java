eb622009fba20064d824b6bae3dce3fb1ebc1917
lingchen336 <473531303@qq.com>
2020-10-07 21:34:14 +0800

    从上到下打印二叉树III(之字形打印)

--- src/com/test/algorithm/BiTree/O_32_III.java
+++ src/com/test/algorithm/BiTree/O_32_III.java
@@ -1,5 +1,14 @@
 package com.test.algorithm.BiTree;
+
+import sun.reflect.generics.tree.Tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 /*
+请实现一个函数按照之字形顺序打印二叉树，即第一行按照从左到右的顺序打印，第二层按照从右到左的顺序打印，第三行再按照从左到右的顺序打印，其他行以此类推。
 
  */
 public class O_32_III {
@@ -9,5 +18,21 @@ public class O_32_III {
         TreeNode right;
         TreeNode(int x) { val = x; }
     }
-
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode>queue=new LinkedList<>();
+        List<List<Integer>> list=new ArrayList<>();
+        if(root!=null) queue.add(root);
+        while (!queue.isEmpty()){
+            LinkedList<Integer>temp=new LinkedList<>();
+            for (int i =queue.size();i>0; i--) {
+                TreeNode flag=queue.poll();
+                if(list.size()%2==0)temp.addLast(flag.val);  // 偶数层 -> 队列头部
+                else temp.addFirst(flag.val);                // 奇数层 -> 队列尾部
+                if(flag.left!=null)queue.add(flag.left);
+                if(flag.right!=null)queue.add(flag.right);
+            }
+            list.add(temp);
+        }
+        return list;
+    }
 }
