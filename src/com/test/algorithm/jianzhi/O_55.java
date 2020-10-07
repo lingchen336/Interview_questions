dca560333bf554c073b107f56f00e33fe0a80aba
lingchen336 <473531303@qq.com>
2020-10-07 09:46:42 +0800

    平衡二叉树

--- src/com/test/algorithm/jianzhi/O_55.java
+++ src/com/test/algorithm/jianzhi/O_55.java
@@ -3,14 +3,15 @@ package com.test.algorithm.jianzhi;
 /*
 输入一棵二叉树的根节点，判断该树是不是平衡二叉树。如果某二叉树中任意节点的左右子树的深度相差不超过1，那么它就是一棵平衡二叉树。
  */
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode(int x) { val = x; }
-}
+
 
 class O_55 {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
     public boolean isBalanced(TreeNode root) {
         return sub(root)==-1;
     }
