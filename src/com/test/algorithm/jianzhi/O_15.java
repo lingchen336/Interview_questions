68a4e0b2bca1339e65b9a71d1975a00b95e09504
lingchen336 <473531303@qq.com>
2020-10-08 10:22:13 +0800

    二进制中1的个数

---
+++ src/com/test/algorithm/jianzhi/O_15.java
@@ -0,0 +1,16 @@
+package com.test.algorithm.jianzhi;
+/*
+请实现一个函数，输入一个整数，输出该数二进制表示中 1 的个数。例如，把 9表示成二进制是 1001，有 2 位是 1。因此，如果输入 9，则该函数输出 2
+
+>>>无符号右移
+ */
+public class O_15 {
+    public int hammingWeight(int n) {
+        int res=0;
+        while (n!=0){
+            res+=n&1;
+            n>>>=1;
+        }
+        return res;
+    }
+}
--- src/com/test/leetcode/Solution.java
+++ src/com/test/leetcode/Solution.java
@@ -1,18 +1,3 @@
 package com.test.leetcode;
 
-public class Solution {
-    class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-        TreeNode(int x) { val = x; }
-    }
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root==null||q==root||p==root)return root;
-        TreeNode rleft=lowestCommonAncestor(root.left,p,q);
-        TreeNode rright=lowestCommonAncestor(root.right,p,q);
-        if(rleft==null)return rright;
-        if(rright==null)return rleft;
-        return root;
-    }
-}
+
