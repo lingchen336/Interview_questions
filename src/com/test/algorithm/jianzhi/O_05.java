dbcb810e3854049bd7cdb26486efd8cd5c16ed9b
lingchen336 <473531303@qq.com>
2020-10-08 14:55:32 +0800

    替换空格

---
+++ src/com/test/algorithm/jianzhi/O_05.java
@@ -0,0 +1,10 @@
+package com.test.algorithm.jianzhi;
+/*
+请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
+
+ */
+public class O_05 {
+    public String replaceSpace(String s) {
+        return s.replace(" ","%20");
+    }
+}
